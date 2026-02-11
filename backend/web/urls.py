135080e574b4dcfd4588e3bac686bed72852bf8e
Taka51期 <sh.takanori@gmail.com>
2026-02-12 01:48:16 +0900

    urls.pyにアップロードテスト用のpath追加

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -50,4 +50,8 @@ urlpatterns = [
         name="category_delete_api",
     ),
     path("api/users", views.users_api, name="users_api"),
+
+    # レシートテスト用
+    path("receipt_test/", views.receipt_test_page),
+    path("receipt_upload_api", views.receipt_upload_api),
 ]
